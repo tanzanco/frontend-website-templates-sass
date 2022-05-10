83a8e9686b9838f9a7f7bd6571544084dec37a1c
tanzanco <104733433+tanzanco@users.noreply.github.com>
2022-05-11 01:40:30 +0530

    Add files via upload

---
+++ Fitness/js/script.js
@@ -0,0 +1,37 @@
+let menu = document.querySelector('#menu-btn');
+let navbar =  document.querySelector('.header .navbar');
+
+
+menu.onclick = () => {
+  menu.classList.toggle('fa-times');
+  navbar.classList.toggle('active');
+};
+
+
+window.onscroll = () => {
+  menu.classList.remove('fa-times');
+  navbar.classList.remove('active');
+};
+
+
+var swiper = new Swiper(".home-slider", {
+    spaceBetween: 20,
+    effect: "fade",
+    grabCursor: true,
+    loop:true,
+    centeredSlides: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
+
+
+  var swiper = new Swiper(".review-slider", {
+    spaceBetween: 20,
+    effect: "fade",
+    grabCursor: true,
+    loop:true,
+    centeredSlides: true,
+    
+  });
